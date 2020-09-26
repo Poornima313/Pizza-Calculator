99621e1a0837dc530937bded03dfd70ee9f0df6f
Poornima313 <poornima31p@gmail.com>
2020-09-26 11:35:07 +0530

    integrating redux

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,9 +1,17 @@
 import React from "react";
 import Title from "./Title";
+import NumberOfPeopleInputContainer from "../containers/NumberOfPeopleInputContainer";
+import SlicesPerPersonInputContainer from "../containers/SlicesPerPersonInputContainer";
+import ResultContainer from "../containers/ResultContainer";
+import ResetButtonContainer from "../containers/ResetButtonContainer";
 
 const Application = () => (
   <div className="Application">
     <Title />
+    <NumberOfPeopleInputContainer />
+    <SlicesPerPersonInputContainer />
+    <ResultContainer />
+    <ResetButtonContainer />
   </div>
 );
 
--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
 import React from "react";
 import { render } from "react-dom";
-import { createStore, compose } from "redux";
+
+import { createStore } from "redux";
 import { Provider } from "react-redux";
 
 import Application from "./components/Application";
+
 import getInitialState from "./initial-state";
 import reducer from "./reducer";
 
 import "./style.css";
-
 const store = createStore(reducer, getInitialState());
-
-// const middleware = [];
 
 render(
   <Provider store={store}>
